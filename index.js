100f15c47df5078a4f86c9f8540369bc6c3c6750
salubrious-commit-8954 <wangyy395@gmail.com>
2020-01-27 20:58:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var result = []
+  for (var i = 0; i < musicians.length; i++) {
+    var temp = ""
+    temp = musicians[i] + " plays " + instruments[i];
+    result.push(temp);
+  }
+  return result;
+}
+
+function johnLennonFacts(facts) {
+
+  var result = [];
+  var counter = 0;
+  while (facts.length > 0) {
+    var temp = "";
+    temp = facts[counter];
+    temp += "!!!";
+    result.push(temp);
+    facts.shift();
+    counter++;
+  }
+  return result;
+}
